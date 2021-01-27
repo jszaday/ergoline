8bc83fddaa6d2f454ec87f9a8df833813f141fad
Justin J. Szaday <szaday2@illinois.edu>
2021-01-27 01:15:52 -0600

    intellectual underpinnings for reductions

---
+++ include/ergoline/reducer.hpp
@@ -0,0 +1,94 @@
+#ifndef __ERGOLINE_REDUCER_HPP__
+#define __ERGOLINE_REDUCER_HPP__
+
+#include <map>
+#include <memory>
+#include <functional>
+
+#include <charm++.h>
+
+namespace ergoline {
+using reducer_fn_t = std::function<void(int, void*)>;
+
+namespace {
+using reducer_id_t = std::size_t;
+using reducer_map_t = std::map<reducer_id_t, reducer_fn_t>;
+
+CkpvDeclare(reducer_id_t, last_reducer_id_);
+CkpvDeclare(reducer_map_t, reducer_map_);
+
+void reduction_client_fn(void* param, void* message) {
+  auto id = reinterpret_cast<reducer_id_t>(param);
+  auto msg = static_cast<CkReductionMsg*>(message);
+  CkAssert(CkpvInitialized(reducer_map_));
+  CkpvAccess(reducer_map_)[id](msg->getLength(), msg->getData());
+}
+
+reducer_id_t get_next_id() {
+  if (!CkpvInitialized(last_reducer_id_)) {
+    CkpvInitialize(reducer_id_t, last_reducer_id_);
+    CkpvAccess(last_reducer_id_) = -1;
+  }
+  return ++CkpvAccess(last_reducer_id_);
+}
+
+reducer_id_t register_reducer(const reducer_fn_t& f) {
+  if (!CkpvInitialized(reducer_map_)) {
+    CkpvInitialize(reducer_map_t, reducer_map_);
+  }
+  auto id = get_next_id();
+  CkpvAccess(reducer_map_)[id] = f;
+  return id;
+}
+
+template <bool B>
+using Requires = PUP::Requires<B>;
+}
+
+class reducer {
+  CkReduction::reducerType type_;
+  CkCallback cb_;
+
+ public:
+  reducer() {}
+  reducer(const reducer& r) : cb_(r.cb_), type_(r.type_) {}
+  reducer(const CkReduction::reducerType& ty, const reducer_fn_t& action)
+      : type_(ty) {
+    auto id = register_reducer(action);
+    new (&cb_) CkCallback(&reduction_client_fn, reinterpret_cast<void*>(id));
+  }
+
+  template <typename T, typename... Args,
+            Requires<(sizeof...(Args) > 1)> = nullptr>
+  void contribute(T* t, const Args&... arguments) {
+    auto args = std::forward_as_tuple(const_cast<Args&>(arguments)...);
+    auto size = PUP::size(args);
+    auto buf = new char[size];
+    PUP::toMemBuf(args, buf, size);
+    t->contribute(size, buf, type_, cb_);
+    delete[] buf;
+  }
+
+  template <typename T, typename Value>
+  void contribute(T* t, const Value& value) {
+    t->contribute(sizeof(Value), &value, type_, cb_);
+  }
+
+  template <typename T>
+  void contribute(T* t, int size, void* data) {
+    t->contribute(size, data, type_, cb_);
+  }
+
+  template <typename T>
+  void contribute(T* t) {
+    t->contribute(cb_);
+  }
+
+  virtual void pup(PUP::er& p) {
+    p | type_;
+    p | cb_;
+  }
+};
+}
+
+#endif
