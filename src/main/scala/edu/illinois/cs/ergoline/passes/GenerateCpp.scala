16b0cfb8b0002b4f7de166c0ab882d4ac487fb8f
wingpad <wingpad@gmail.com>
2021-03-24 22:05:51 -0500

    minor correctness fixes for SMP mode

--- src/main/scala/edu/illinois/cs/ergoline/passes/GenerateCpp.scala
+++ src/main/scala/edu/illinois/cs/ergoline/passes/GenerateCpp.scala
@@ -297,13 +297,13 @@ object GenerateCpp extends EirVisitor[CodeGenerationContext, Unit] {
       case "get" =>
         ctx << "(([&](const ergoline::future& f)" << "{"
         ctx << "// TODO use a reqman here instead ;"
-        ctx << "CthThread sleeper = nullptr;"
-        if (ptr) {
-          ctx << ctx.typeFor(ty, Some(base)) << "tmp;"
-        } else {
-          ctx << "ergoline::temporary<" << ctx.typeFor(ty, Some(base)) << "> tmp;"
-        }
-        ctx << "auto req = this->__make_future_req__(f, [&](std::shared_ptr<CkMessage>& msg)" << "{"
+        ctx << "using temporary_type = "
+        if (ptr) ctx << ctx.typeFor(ty, Some(base)) << ";"
+        else ctx << "ergoline::temporary<" << ctx.typeFor(ty, Some(base)) << ">;"
+        ctx << "auto rec = std::make_shared<std::tuple<CthThread, temporary_type>>();"
+        ctx << "auto &sleeper = std::get<0>(*rec);"
+        ctx << "auto &tmp = std::get<1>(*rec);"
+        ctx << "auto req = this->__make_future_req__(f, [=](std::shared_ptr<CkMessage>& msg)" << "{"
         ctx << "ergoline::unpack(std::move(msg), tmp);"
         ctx << "if (sleeper != nullptr) CthAwaken(sleeper);"
         ctx << "return true;"
