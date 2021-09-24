6e8c2ac25bad292146ecc837a16982f62ab8fbb2
Justin J. Szaday <szaday2@illinois.edu>
2021-09-23 20:59:18 -0400

    Add Default Codepath

--- include/ergoline/array.hpp
+++ include/ergoline/array.hpp
@@ -21,9 +21,10 @@ inline std::size_t reduce(const std::array<std::size_t, N> &shape) {
     return tmp;
   }
 }
-} // namespace detail
+}  // namespace detail
 
-template <typename T, std::size_t N> struct nd_span {
+template <typename T, std::size_t N>
+struct nd_span {
   static_assert(N >= 0, "dimensionality must be positive");
 
   using shape_type = std::array<std::size_t, N>;
@@ -51,8 +52,8 @@ template <typename T, std::size_t N> struct nd_span {
     return *(this->begin() + idx);
   }
 
-  inline static std::size_t
-  total_size(const std::array<std::size_t, N> &shape) {
+  inline static std::size_t total_size(
+      const std::array<std::size_t, N> &shape) {
     return offset + detail::reduce(shape) * sizeof(T);
   }
 
@@ -63,12 +64,12 @@ template <typename T, std::size_t N> struct nd_span {
 
   static void operator delete(void *ptr) { std::free(ptr); }
 
-private:
+ private:
   nd_span(const std::array<std::size_t, N> &_) : shape(_) {}
 
-public:
-  static std::shared_ptr<nd_span<T, N>>
-  instantiate(const std::array<std::size_t, N> &shape) {
+ public:
+  static std::shared_ptr<nd_span<T, N>> instantiate(
+      const std::array<std::size_t, N> &shape) {
     return std::shared_ptr<nd_span<T, N>>(new (shape) nd_span<T, N>(shape));
   }
 
@@ -81,9 +82,8 @@ public:
 
 template <>
 template <>
-std::shared_ptr<nd_span<double, 2>>
-nd_span<double, 2>::fill<int, int>(const std::tuple<int, int> &tup,
-                                   const double &value) {
+std::shared_ptr<nd_span<double, 2>> nd_span<double, 2>::fill<int, int>(
+    const std::tuple<int, int> &tup, const double &value) {
   auto span = instantiate(
       {(std::size_t)std::get<0>(tup), (std::size_t)std::get<1>(tup)});
   std::fill(span->begin(), span->end(), value);
@@ -91,17 +91,23 @@ nd_span<double, 2>::fill<int, int>(const std::tuple<int, int> &tup,
 }
 
 template <>
-std::shared_ptr<nd_span<double, 1>>
-nd_span<double, 1>::fill(const int &size, const double &value) {
+std::shared_ptr<nd_span<double, 1>> nd_span<double, 1>::fill(
+    const int &size, const double &value) {
   auto span = instantiate({(std::size_t)size});
   std::fill(span->begin(), span->end(), value);
   return span;
 }
 
-template <typename T, std::size_t N> using array = nd_span<T, N>;
-} // namespace ergoline
+template <typename T, std::size_t N>
+using array = nd_span<T, N>;
+}  // namespace ergoline
 
 namespace hypercomm {
+
+template <class T, std::size_t N>
+struct is_idiosyncratic_ptr<ergoline::nd_span<T, N>> {
+  static constexpr auto value = true;
+};
 
 template <typename T, std::size_t N>
 struct is_zero_copyable<ergoline::nd_span<T, N>> {
@@ -137,40 +143,25 @@ struct zero_copy_fallback<ergoline::nd_span<T, N>,
   }
 };
 
-template <typename T, std::size_t N> struct puper<ergoline::nd_span<T, N>> {
-  inline static void impl(serdes &s, ergoline::nd_span<T, N> &t) {
-    CkAbort("not implemented!");
-    // if (s.unpacking()) {
-    //   reconstruct(&t);
-    // }
-
-    // pup(s, t.shape);
-
-    // if (s.unpacking()) {
-    //   if (PUP::as_bytes<T>::value) {
-    //     t.source = s.observe_source();
-    //     if ((bool)t.source) {
-    //       t.buffer = reinterpret_cast<T*>(s.current);
-    //       s.advance<T>(t.size());
-    //     } else {
-    //       t.alloc(false, false);
-    //       s.copy(t.buffer, t.size());
-    //     }
-    //   } else {
-    //     t.alloc(true, true);
-    //     for (auto& i : t) {
-    //       pup(s, i);
-    //     }
-    //   }
-    // } else if (PUP::as_bytes<T>::value) {
-    //   s.copy(t.buffer, t.size());
-    // } else {
-    //   for (auto& i : t) {
-    //     pup(s, i);
-    //   }
-    // }
+template <typename T, std::size_t N>
+struct puper<std::shared_ptr<ergoline::nd_span<T, N>>,
+             typename std::enable_if<!is_bytes<T>()>::type> {
+  using shape_type = typename ergoline::nd_span<T, N>::shape_type;
+  inline static void impl(serdes &s,
+                          std::shared_ptr<ergoline::nd_span<T, N>> &t) {
+    if (s.unpacking()) {
+      auto *shape = reinterpret_cast<shape_type *>(s.current);
+      s.advanceBytes(sizeof(shape_type));
+      new (&t) std::shared_ptr<ergoline::nd_span<T, N>>(
+          ergoline::nd_span<T, N>::instantiate(*shape));
+    } else {
+      s | t->shape;
+    }
+    for (auto &elt : *t) {
+      s | elt;
+    }
   }
 };
-} // namespace hypercomm
+}  // namespace hypercomm
 
 #endif
