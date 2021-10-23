d6f6195fea65ec8f8a3fa5c46e362855b379cba5
Justin Szaday <wingpad@gmail.com>
2021-10-22 22:44:15 -0400

    Revert "Test Updates to Hypercomm (#156)"
    This reverts commit d8a2a57dea5e7fbf17b2b6b14f5efa26010861ba.

--- include/ergoline/mailbox.hpp
+++ include/ergoline/mailbox.hpp
@@ -6,17 +6,7 @@
 namespace ergoline {
 
 template<typename... Ts>
-struct compactor_ {
-  using type = std::tuple<std::tuple<Ts...>>;
-};
-
-template<typename T>
-struct compactor_<T> {
-  using type = std::tuple<T>;
-};
-
-template<typename... Ts>
-using compact_t = typename compactor_<Ts...>::type;
+using compact_t = std::tuple<std::tuple<Ts...>>;
 
 template<typename... Ts>
 class mailbox : public hypercomm::mailbox<compact_t<Ts...>> {
--- src/main/scala/edu/illinois/cs/ergoline/passes/GenerateCpp.scala
+++ src/main/scala/edu/illinois/cs/ergoline/passes/GenerateCpp.scala
@@ -22,10 +22,7 @@ import edu.illinois.cs.ergoline.resolution.{
   EirTemplateFacade,
   Find
 }
-import edu.illinois.cs.ergoline.util.EirUtilitySyntax.{
-  RichOption,
-  RichResolvableTypeIterable
-}
+import edu.illinois.cs.ergoline.util.EirUtilitySyntax.RichOption
 import edu.illinois.cs.ergoline.util.TypeCompatibility.RichEirClassLike
 import edu.illinois.cs.ergoline.util.{Errors, assertValid, onLeftSide}
 import edu.illinois.cs.ergoline.util
@@ -582,13 +579,7 @@ object GenerateCpp extends EirVisitor[CodeGenerationContext, Unit] {
         if (fc.exists(ctx.shouldRepack)) ctx << "ergoline::repack("
         else ctx << "hypercomm::pack("
       case Some((true, tys)) =>
-        ctx << "hypercomm::make_typed_value<"
-        if (tys.length == 1) {
-          ctx << tys.head
-        } else {
-          ctx << "std::tuple<" << (tys, ",") << ">"
-        }
-        ctx << ">("
+        ctx << "hypercomm::make_typed_value<std::tuple<" << (tys, ",") << ">>("
       case _ =>
     }
 
@@ -2976,7 +2967,8 @@ object GenerateCpp extends EirVisitor[CodeGenerationContext, Unit] {
     if (compound && x.condition.isDefined) ???
 
     def encapsulate(types: List[EirResolvable[EirType]]): EirType = {
-      types.toTupleType(allowUnit = true)(None).asInstanceOf[EirType]
+      if (types.nonEmpty) EirTupleType(None, types)
+      else globals.unitType
     }
 
     val nPorts = x.patterns.length
@@ -3020,25 +3012,17 @@ object GenerateCpp extends EirVisitor[CodeGenerationContext, Unit] {
     ctx << "auto" << com << "=" << "ergoline::make_component<" << (set + "type__") << ">(*this," << nPorts.toString << ","
     ctx << "[=](" << (set + "type__") << "&" << set << ")" << "{"
 
-    val isTuple = x.patterns.length != 1
     x.patterns.zipWithIndex.foreach({ case ((_, patterns), i) =>
       val (m, mboxName, _, arrayArgs) = quadruplets(i)
       val name = s"__value${i}__"
       val ty = name.init + "type__"
 
-      ctx << "auto" << name << "=" << {
-        if (isTuple) {
-          s"std::move($set);"
-        } else {
-          s"std::move(std::get<$i>($set));"
-        }
-      }
-
+      ctx << "auto" << name << "=" << s"std::move(std::get<$i>($set));"
       ctx << visitPatternDecl(
         ctx,
         patterns,
         name + "->value()",
-        forceTuple = isTuple
+        forceTuple = true
       ).split(n)
 
       if (arrayArgs.nonEmpty) {
@@ -3049,13 +3033,13 @@ object GenerateCpp extends EirVisitor[CodeGenerationContext, Unit] {
     ctx << visitOptionalStatement(x.body) << "}" << ");"
 
     x.patterns.zipWithIndex.foreach({ case ((_, patterns), i) =>
-      val (_, mboxName, resolved, _) = quadruplets(i)
+      val (m, mboxName, resolved, arrayArgs) = quadruplets(i)
       val temp = ctx.temporary
       val tempVal = s"$temp->value()"
       val declarations =
-        visitPatternDecl(ctx, patterns, tempVal, forceTuple = isTuple).split(n)
-      val conditions = visitPatternCond(ctx, patterns, tempVal, Some(resolved))
-        .mkString(" && ")
+        visitPatternDecl(ctx, patterns, tempVal, forceTuple = true).split(n)
+      val conditions =
+        visitPatternCond(ctx, patterns, tempVal, Some(resolved)).mkString(" && ")
 
       val pred: Option[String] = Option.when(conditions.nonEmpty)({
         val name = s"__pred${i}__"
